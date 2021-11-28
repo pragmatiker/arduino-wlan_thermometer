514c0f57e56d27ce967e92dac3b3e47e85cbe5c1
Tim Herty <tim.herty@gmail.com>
2021-11-28 08:43:57 +0100

    neue Datei:     arduino-wlan_thermometer.ino

---
+++ arduino-wlan_thermometer.ino
@@ -0,0 +1,140 @@
+/* DHTServer - ESP8266 Webserver with a DHT sensor as an input
+
+   Based on ESP8266Webserver, DHTexample, and BlinkWithoutDelay (thank you)
+
+   Version 1.0  5/3/2014  Version 1.0   Mike Barela for Adafruit Industries
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>        // Include the mDNS library
+#include <DHT.h>
+#include "arduino-secrets.h"
+
+#define DHTTYPE DHT11
+#define DHTPIN  2
+
+const char* ssid     = WIFI_SSID;
+const char* password = WIFI_PASSWD;
+
+ESP8266WebServer server(80);
+ 
+// Initialize DHT sensor 
+// NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
+// you need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// This is for the ESP8266 processor on ESP-01 
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
+ 
+float humidity, temp_c;  // Values read from sensor
+String webString="";     // String to display
+// Generally, you should use "unsigned long" for variables that hold time
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 2000;              // interval at which to read sensor
+  
+void setup(void)
+{
+  // You can open the Arduino IDE Serial Monitor window to see what the code is doing
+  Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
+  dht.begin();           // initialize temperature sensor
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.print("\n\r \n\rWorking to connect");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("DHT Weather Reading Server");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+   
+  server.on("/", handle_root);  
+  server.begin();
+  Serial.println("HTTP server started");
+  
+  if (!MDNS.begin("garage")) {             // Start the mDNS responder for esp8266.local
+    Serial.println("Error setting up MDNS responder!");
+  }
+  Serial.println("mDNS responder started");
+}
+ 
+void loop(void) {
+  server.handleClient();
+} 
+
+void handle_root() {
+    gettemperature(); // read sensor
+    server.send(200, "text/html", SendHTML(temp_c,humidity)); // send to someones browser when asked
+}
+
+void gettemperature() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor 
+    previousMillis = currentMillis;   
+
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    temp_c = dht.readTemperature();     // Read temperature as Fahrenheit
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temp_c)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+  }
+}
+
+String SendHTML(float Temperaturestat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<meta http-equiv=\"refresh\" content=\"5\">\n";
+  ptr +="<title>ESP8266 Weather Report</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>Weather Report</h1>\n";
+  
+  ptr +="<p>Temperature: ";
+  if ((int)Temperaturestat > 30) {
+    ptr +="<span style=\"color:red;\">";
+  }  
+  else if ((int)Temperaturestat < 30 && (int)Temperaturestat > 25 ) {
+    ptr +="<span style=\"color:orange;\">";
+  }
+  else if ((int)Temperaturestat < 25 && (int)Temperaturestat > 18 ) {
+    ptr +="<span style=\"color:green;\">";
+  }
+  else {
+    ptr +="<span style=\"color:blue;\">";
+  }
+  ptr += (int)Temperaturestat;
+  ptr += "</span>";
+  ptr +="&#8451</p>";
+  ptr +="<p>Humidity: ";
+  ptr +=(int)Humiditystat;
+  ptr +="%</p>";
+  
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
